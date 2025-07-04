3ea40bbe5b7f03b318c13c8b920807ad1f450a52
Uddeshy Manoj Jaiswal <uddeshkrrish@gmail.com>
2025-07-04 10:41:51 +0530

    created folder structure

---
+++ client/knovatorclientapp/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'  
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div> 
+       The Knovator Assignment Client App
+      </div>
+      <p >
+       
+      </p>
+    </>
+  )
+}
+
+export default App
